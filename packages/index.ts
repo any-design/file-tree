2be134659548261a36eb33cf673581d16905f8bf
BackRunner <dev@backrunner.top>
2024-03-29 18:38:42 +0800

    chore: export context menu

--- packages/index.ts
+++ packages/index.ts
@@ -1,4 +1,7 @@
 // packages/index.ts
 import FileTree from './filetree/FileTree.vue'; // 引入封装好的组件
-export { FileTree };
+import ContextMenu from './contextmenu/ContextMenu.vue'; // 引入封装好的组件
+import ContextMenuItem from './contextmenu/ContextMenuItem.vue'; // 引入封装好的组件
+
+export { FileTree, ContextMenu, ContextMenuItem };
 export * from './filetree/types';
